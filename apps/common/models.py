57b734cfddc18a3866f6420dc464762b15516267
Mohammad Rezazade <sepydev@gmail.com>
2024-05-25 15:41:24 +0300

    refactor: remove unused models

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import F, Q
 from django.utils import timezone
 
 
@@ -9,36 +8,3 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
-
-
-class SimpleModel(models.Model):
-    """
-    This is a basic model used to illustrate a many-to-many relationship
-    with RandomModel.
-    """
-
-    name = models.CharField(max_length=255, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-class RandomModel(BaseModel):
-    """
-    This is an example model, to be used as reference in the Styleguide,
-    when discussing model validation via constraints.
-    """
-
-    start_date = models.DateField()
-    end_date = models.DateField()
-
-    simple_objects = models.ManyToManyField(
-        SimpleModel, blank=True, related_name="random_objects"
-    )
-
-    class Meta:
-        constraints = [
-            models.CheckConstraint(
-                name="start_date_before_end_date", check=Q(start_date__lt=F("end_date"))
-            )
-        ]
