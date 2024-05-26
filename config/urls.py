018d984e1e808b9e60628162e90a3c9648fff50d
Mohammad Rezazade <sepydev@gmail.com>
2024-05-26 10:57:20 +0300

    feat: config HTMX, Tailwind, daisyUI

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,16 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("auth/", include("apps.authentication.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("auth/", include("apps.authentication.urls")),
+        path("admin-panel/", include("apps.admin_panel.urls")),
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
 
 from config.settings.debug_toolbar.setup import DebugToolbarSetup  # noqa
 
