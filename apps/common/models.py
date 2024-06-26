9312fa85169fbb4020a20b50da3dd9eda18d888b
Mohammad Rezazade <sepydev@gmail.com>
2024-06-26 10:38:05 +0300

    feat: implement time stamped model

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from django.utils import timezone
 
 
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(db_index=True, default=timezone.now)
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
+
+
+BaseModel = TimeStampedModel
